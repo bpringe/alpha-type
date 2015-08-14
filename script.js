dbe03ff90454904baadd07e0e775d3463421f4ee
Brandon Ringe <brandon.ringe@gmail.com>
2015-08-13 19:29:52 -0500

    - Add divBlock class, divBlock array, and animate divBlocks in array - Remove divBlock from array when it hits the bottom of the canvas - Add number of divBlocks to stats at top of screen - update real time

--- script.js
+++ script.js
@@ -1,52 +1,82 @@
-var CANVAS_WIDTH = 1200;
-var CANVAS_HEIGHT = 760;
-var timer = 0;
+var CANVAS_WIDTH = 850;
+var CANVAS_HEIGHT = 400;
+var updates = 0;
+var divArray = [];
+
+var DivBlock = function() {
+	this.width = 30;
+	this.height = 35;
+	this.topOffset = 0;
+	this.leftOffset = Math.floor(Math.random() * (CANVAS_WIDTH - this.width));
+	this.content = "C";
+};
+DivBlock.prototype.draw = function () {
+	$("#canvas").append("<div class='divBlock' style='left:"
+	+ this.leftOffset + "px;top:" + this.topOffset + "px;'><span class='divContent'>C</span></div>");
+};
+
+function clearCanvas() {
+	$("#canvas").empty();
+}
+
+function clearDivArray() {
+	divArray = [];
+}
 
 function initializeCanvas() {
 	$("#canvas").width(CANVAS_WIDTH);
 	$("#canvas").height(CANVAS_HEIGHT);
 }
 
-// test: timer needs to be changed to avoid memory overflow from high number
-function updateTimer() {
-	$("#time").text(timer);
-	timer++;
-	if (timer > 50000) {
-		timer = 0;
+function drawDivArray() {
+	clearCanvas();
+	for (i = 0; i < divArray.length; i++) {
+		divArray[i].topOffset += 1;
+		divArray[i].draw();
+		if ((divArray[i].topOffset + divArray[i].height) >= CANVAS_HEIGHT) {
+			divArray.splice(i, 1);
+			drawDivArray();
+		}
 	}
+}
+
+// test: updates needs to be changed to avoid memory overflow from high number
+function updateGame() {
+	$("#updates").text("Updates: " + updates);
+	updates++;
+	$("#numDivs").text("# divs: " + $(".divBlock").length);
+	drawDivArray();
 }
 
 // This is called in $(document).ready
 function startGame() {
 	initializeCanvas();
-	setInterval(updateTimer, (1000/60));  // (1000/60 = 60fps)
+	setInterval(updateGame, (1000/60));  // (1000/60 = 60fps)
+	
+	// Get mouse X and Y coordinates within the canvas
+	// This is pointless for now...
 	$("#canvas").mousemove(function(event) {
 		var parentOffset = $(this).offset();
 		var relX = event.pageX - parentOffset.left;
 		var relY = event.pageY - parentOffset.top;
-		$("#coordinates").text("(" + relX + ", " + relY + ")");
+		$("#coordinates").text("Mouse (x, y): (" + relX + ", " + relY + ")");
 	});
-}
-
-function clearCanvas() {
-	$("#canvas").empty();
-}
-
-function spawnDiv() {
-	$("#canvas").append("<div>Another div</div>");
 }
 
 // Load the game once the document is finished loading
 $(document).ready(function() {
 	startGame();
+	
 	$(document).keydown(function(event) {
 		console.log("Handler for .keydown() called. " + event.keyCode); // test
-		// to-do: add switch statement for keyCodes
-		if (event.keyCode === 90) {
-			spawnDiv();
-		}
-		if (event.keyCode === 67) {
-			clearCanvas();
+		switch (event.keyCode) {
+			case 90: // z
+					var divBlock = new DivBlock();
+					divArray.push(divBlock);
+					break;
+			case 67: // c
+					clearDivArray();
+					break;
 		}
 	});
 });
