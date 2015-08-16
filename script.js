9237336b25647bc4fb0015fad65ab66e963f3461
Brandon Ringe <brandon.ringe@gmail.com>
2015-08-15 19:28:39 -0500

    - Add game difficulty - decrease number of updates between spawns 		every set amount of points acquired - Refactor code to cleanup unnecessary stats tracking and combine 		init functions into initGame() function - Change character set to include only letters - Set score positioning with JS according to canvas positioning

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var CANVAS_WIDTH = 850;
-var CANVAS_HEIGHT = 200;
+var CANVAS_HEIGHT = 400;
 var CANVAS_TOP_OFFSET = 50;
 var CANVAS_LEFT_OFFSET = 50;
 var updates = 0;
 var divArray = [];
-var charSet = '123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var divSpawnRate = 60; // number of updates between spawns
 var score = 0;
 var gameOverProg = 0;
@@ -12,44 +12,27 @@ var gameOverIncrement = 10; // percent toward game over when div hits bottom
 var updateInterval;
 var drawScreenInterval;
 
-// Gets a random character from the character set
-function getRandomChar() {
-	var c = charSet[Math.floor(Math.random() * charSet.length)];
-	return c;
-}
-
 // Javascript "OOP" implementationzz
-var DivBlock = function() {
+var Div = function() {
 	this.width = 30;
 	this.height = 35;
 	this.topOffset = 0;
 	this.leftOffset = Math.floor(Math.random() * (CANVAS_WIDTH - this.width));
 	this.speed = 1; // pixels per update
-	this.content = getRandomChar();
+	this.content = charSet[Math.floor(Math.random() * charSet.length)]; // random char
 };
-DivBlock.prototype.draw = function () {
+Div.prototype.draw = function () {
 	$("#canvas").append("<div class='divBlock' style='left:"
 	+ this.leftOffset + "px;top:" + this.topOffset + "px;'><span class='divContent'>"
 	+ this.content + "</span></div>");
 };
 
+function drawGameOverProg() {
+	$("#gameOverProg").css({"width": gameOverProg + "%"});
+}
+
 function clearCanvas() {
 	$("#canvas").empty();
-}
-
-function clearScreen() {
-	divArray = [];
-	gameOverProg = 0;
-}
-
-function initCanvas() {
-	$("#canvas").css({"width": CANVAS_WIDTH + "px", "height": CANVAS_HEIGHT + "px",
-		"top": CANVAS_TOP_OFFSET + "px", "left": CANVAS_LEFT_OFFSET});
-}
-
-function initGameOverProgDiv() {
-	$("#gameOverProgDiv").css({"top": CANVAS_TOP_OFFSET + CANVAS_HEIGHT + 20 + "px", 
-		"left": CANVAS_LEFT_OFFSET + "px", "width": CANVAS_WIDTH + "px"});
 }
 
 function drawDivArray() {
@@ -60,13 +43,7 @@ function drawDivArray() {
 }
 
 function drawStats() {
-	$("#updates").text("Updates: " + updates);
-	$("#numDivs").text("# divs: " + divArray.length);
 	$("#score").text("Score: " + score);
-}
-
-function drawGameOverProg() {
-	$("#gameOverProg").css({"width": gameOverProg + "%"});
 }
 
 function drawScreen() {
@@ -76,20 +53,17 @@ function drawScreen() {
 }
 
 function gameOver() {
+	clearInterval(updateInterval);
+	clearInterval(drawScreenInterval);
 	$("#canvas").append("<div id='gameOverMessage'>Game Over</div>");
 }
 
-// test: updates needs to be changed to avoid memory overflow from high number
 function updateGame() {
-	/* If game over progress is at 100% or more then stop updating 
-	  and drawing and call game over function */
 	if (gameOverProg >= 100) {
-		clearInterval(updateInterval);
-		clearInterval(drawScreenInterval);
 		gameOver();
 	}
 	
-	// Update stats
+	// Keep track of number of updates
 	updates++;
 	
 	// Update divs
@@ -108,40 +82,44 @@ function updateGame() {
 	
 	// Spawn new divs
 	if ((updates % divSpawnRate) === 0) {
-		var div = new DivBlock();
+		var div = new Div();
 		divArray.push(div);
 	}
 }
 
-// This is called in $(document).ready
-function startGame() {
-	initCanvas();
-	initGameOverProgDiv();
+function clearScreen() {
+	divArray = []; // clears the divs on the canvas
+	gameOverProg = 0; // clears the game over progress bar
+	score = 0;
+}
+
+function initGame() {
+	// Initialize canvas
+	$("#canvas").css({"width": CANVAS_WIDTH + "px", "height": CANVAS_HEIGHT + "px",
+		"top": CANVAS_TOP_OFFSET + "px", "left": CANVAS_LEFT_OFFSET});
+	
+	// Initialize game over progress bar
+	$("#gameOverProgDiv").css({"top": CANVAS_TOP_OFFSET + CANVAS_HEIGHT + 20 + "px", 
+		"left": CANVAS_LEFT_OFFSET + "px", "width": CANVAS_WIDTH + "px"});
+	
+	// Initialize score
+	$("#score").css({"left": CANVAS_LEFT_OFFSET + "px"});
 	
 	// start intervals for calling update and draw functions
 	updateInterval = setInterval(updateGame, (1000/60));  // (1000/60 = 60fps)
 	drawScreenInterval = setInterval(drawScreen, (1000/60));
-	
-	// Get mouse X and Y coordinates within the canvas
-	// This is pointless for now...
-	$("#canvas").mousemove(function(event) {
-		var parentOffset = $(this).offset();
-		var relX = event.pageX - parentOffset.left;
-		var relY = event.pageY - parentOffset.top;
-		$("#coordinates").text("Mouse (x, y): (" + relX + ", " + relY + ")");
-	});
 }
 
 // Load the game once the document is finished loading
 $(document).ready(function() {
-	startGame();
+	initGame();
 	
 	// Test controls
 	$(document).keydown(function(event) {
 		//console.log("Handler for .keydown() called. " + event.keyCode); // test
 		switch (event.keyCode) {
 			case 17: // Ctrl
-					divArray.push(new DivBlock());
+					divArray.push(new Div());
 					break;
 			case 16: // Shift
 					clearScreen();
@@ -156,6 +134,9 @@ $(document).ready(function() {
 			if (event.keyCode === divArray[i].content.charCodeAt(0)) {
 				divArray.splice(i, 1);
 				score++;
+				if (score % 10 === 0) {
+					divSpawnRate -= 2; // number of updates between spawns
+				}
 				/* break to prevent deletion of more than one element of array
 				   in case of multiple same characters on canvas at once */
 				break;  
